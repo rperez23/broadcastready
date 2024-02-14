3b721c6743a9a9c9b4a714265d9a9ce97be483b2
rperez23 <ronnie.lperez@gmail.com>
2024-02-14 14:30:24 -0800

    v001

---
+++ getcaptions.py
@@ -0,0 +1,36 @@
+#! /usr/bin/python3
+
+import pandas as pd
+import re
+from urllib.request import urlretrieve
+import warnings
+
+warnings.filterwarnings('ignore', category=UserWarning, module='openpyxl')
+
+xlf = 'Broadcast-Ready-Status.xlsx'
+tab = 'Sheet'
+
+datareport = pd.read_excel(xlf,tab,usecols='A:E')
+
+for i in range(0,len(datareport)):
+
+	capexists = datareport['S3 HN CAPTION'][i]
+	capurl    = datareport['ASSET CAPTION'][i]
+	hn        = datareport['HOUSE NUMBER'][i]
+
+	if (capexists == 'NO') and (str(capurl) != 'nan'):
+
+		print('downloading:',hn)
+		print(capurl)
+
+		ext = '.scc'
+		msrt = re.search('\.srt?',capurl)
+
+		if msrt:
+			ext = '.srt'
+
+
+		fname = hn + ext
+
+		urlretrieve(capurl,fname)
+
